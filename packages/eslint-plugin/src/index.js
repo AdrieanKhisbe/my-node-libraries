7738370473cba3f33de98d452a91fe2a407fae43
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-10-11 00:14:58 +0200

    Apply the formating and tweak the test :wrench:

--- packages/eslint-plugin/src/index.js
+++ packages/eslint-plugin/src/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const stripComments = require('strip-json-comments');
 
-const CONFIG_FILE = path.join(__dirname, '.eslintrc')
+const CONFIG_FILE = path.join(__dirname, '.eslintrc');
 
-const eslintConfig = JSON.parse(stripComments(fs.readFileSync(CONFIG_FILE).toString()))
+const eslintConfig = JSON.parse(stripComments(fs.readFileSync(CONFIG_FILE).toString()));
 
 module.exports = {
-    configs: {base: eslintConfig}
+  configs: {base: eslintConfig}
 };
--- packages/eslint-plugin/test/full-integration-check.js
+++ packages/eslint-plugin/test/full-integration-check.js
@@ -1,25 +1,28 @@
-const test = require('ava');
 const {exec} = require('child_process');
-const {sumBy, size} = require('lodash/fp');
+const test = require('ava');
+const {sumBy, size, isNil} = require('lodash/fp');
 
-const lint = file => new Promise(resolve => {
-    exec(`npm run -s lint -- ${__dirname}/sample/${file}.js`, (err, stdout, stderr) => {
-        resolve({err, stdout, stderr});
+const lint = file =>
+  new Promise(resolve => {
+    exec(`npm run -s _lint -- --no-ignore ${__dirname}/sample/${file}.js`, (err, stdout, stderr) => {
+      resolve({err, stdout, stderr});
     });
-});
+  });
 
 test('Some perfectly working file', async t => {
-    const lintResult = await lint('ok');
-    t.is(lintResult.err, null);
+  const lintResult = await lint('ok');
+  t.is(lintResult.err, null);
 });
 
 test('Some messy broken file', async t => {
-    const lintResult = await lint('ko');
-    t.true(lintResult.err.message.startsWith('Command failed'));
-    const lintOutput = lintResult.stdout.split('\n');
-    const countOccurences = pattern => sumBy(line => size(line.match(new RegExp(pattern, 'g'))), lintOutput);
+  const lintResult = await lint('ko');
+  t.false(isNil(lintResult.err));
+  t.true(lintResult.err.message.startsWith('Command failed'));
+  const lintOutput = lintResult.stdout.split('\n');
+  const countOccurences = pattern =>
+    sumBy(line => size(line.match(new RegExp(pattern, 'g'))), lintOutput);
 
-    t.is(countOccurences(':\\d+\\s+error'), 3);
-    t.is(countOccurences('prettier/prettier'), 2);
-    t.is(countOccurences('prefer-const'), 1);
+  t.is(countOccurences(':\\d+\\s+error'), 3);
+  t.is(countOccurences('prettier/prettier'), 2);
+  t.is(countOccurences('prefer-const'), 1);
 });
